8ec8d23e0f06fb42cc59adca79169abaced3beab
GlennStewart125 <g.c.w.stewart@students.uu.nl>
2014-03-01 15:12:51 +0100

    links zo aangepast dat ze nu ook door middel van de cijfers terug linken naar het gedeelte waar het cijfer in voor kwam

--- js/site.js
+++ js/site.js
@@ -15,18 +15,22 @@ $(function() {
 $(function() { 
 	var hrefs = [];
 	var spul ="";
+	var ouder=[]; 
 		$('#tekst').find('a').each(function() 
 		{
 			hrefs.push($(this).attr('href'));
+			ouder.push($(this).parent().parent().parent().attr('id'));
+			console.log(ouder);
 		})	
 		jQuery.each(hrefs, function(i, val) {
-          spul +=  '<p><a href=' + hrefs[i] + '>' + '[' +(i+1) + ']' + " " + hrefs[i] + '</a></p><hr><br>';
-		  console.log(spul);
+			//jQuery.each(ouder, function(p, val) {
+			//spul +=  '<p><a href=' + hrefs[i] + '>' + '[' +(i+1) + ']' + " " + hrefs[i] + '</a></p><hr><br>';
+			spul +=  '<p><a href=' + "#" + ouder[i] + '>' + '[' + (i) + ']' + '</a>' +'&nbsp' + '<a href=' + hrefs[i] + '>' +  hrefs[i] + '</a></p><hr><br>';
+			//console.log(spul);
+			//})
         });
-       $('.s16 div').html(spul);
+       $('#sref div').html(spul);
 })
 
-
-
-
-
+//spul +=  '<p><a href=' + "#" + ouder[i] + '>' + '[' + (i) + ']' + '</a>' + '<a href=' + hrefs[i] + '>' +  hrefs[i] + '</a></p><hr><br>';
+//spul +=  '<p><a href=' + "#" + ouder[i] + "&nbsp" + "s" + i + '>' + '[' + (i) + ']' + '</a>' + '<a href=' + hrefs[i] + '>' +  hrefs[i] + '</a></p><hr><br>';
