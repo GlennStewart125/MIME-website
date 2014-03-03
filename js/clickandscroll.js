0e7d59f5bf45b9d860831aa0f4f10f4c78644fb3
Ralphhorn <horn.ralph@gmail.com>
2014-03-03 20:24:02 +0100

    Script voor content 1 aanpassen in header.html

--- js/clickandscroll.js
+++ js/clickandscroll.js
@@ -1,8 +1,7 @@
 //Smooth scrolling function
 $(document).ready(function($) {
 	$(".scrollto").click(function(scroller){
-		scroller.preventDefault();
-		
+		scroller.preventDefault();	
 		$("html,body").animate({scrollTop: $(this.hash).offset().top - 120}, 1500);
 		location.hash = this.hash;
 	});
